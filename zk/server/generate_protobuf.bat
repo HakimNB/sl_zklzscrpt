82557ec9ff6a73296dc79b708835d6f04f64faec
Hakim Hauston <jibostore@gmail.com>
2018-08-17 15:48:00 +0800

    [zoo] server - generate protobuf files and copy to client project - fix wrong methods

--- zk/server/generate_protobuf.bat
+++ zk/server/generate_protobuf.bat
@@ -1,6 +1,7 @@
 call ..\config.bat
 pushd %ZKDIR%\server\Tools\protoc-2.6.1-win32
 call GenProtocolProj.bat
-copy ..\..\ProtoBuf\Include\* ..\..\..\client\libProto\Include
-copy ..\..\ProtoBuf\Source\* ..\..\..\client\libProto\Source
+popd
+pushd %ZKDIR%\client\Tools\protoc-2.6.1-win32
+call GenProtocolProj.bat
 popd
